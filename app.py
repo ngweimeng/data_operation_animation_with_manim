e13e6435672d33798de3511338ef880fbdb294f5
Ng Wei Meng <ngwm96@gmail.com>
2024-09-15 22:14:14 +0200

    adjust webpage layout

--- app.py
+++ app.py
@@ -128,8 +128,8 @@ st.markdown(
     }
     /* Reduce padding around main content */
     .main .block-container {
-        padding-top: 1.5rem;
-        padding-bottom: 1rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
     }
     </style>
     """,
